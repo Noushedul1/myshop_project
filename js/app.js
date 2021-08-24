ce68858520bcefde5d7dce5eabcf54cefc212af3
Md.Noushedul Islam <noushedakib@gmail.com>
2021-08-24 13:07:44 +0600

    Home page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var hamburger = document.getElementById('hamburger');
+hamburger.addEventListener('click', function(){
+	// var lorem = document.getElementById("lorem");
+	// console.log(lorem.classList.toggle('show'));
+
+	var navUl = document.getElementById('nav-ul');
+	console.log(navUl.classList.toggle('show'));
+})
